1d95861e5a269d2e88277f54b75ae733ee77fb27
Nurzhan Zhakia <220760@astanait.edu.kz>
2025-01-30 17:23:51 +0500

    users removed

--- src/app/users/[id].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router'
-
-const User = () => {
-  const router = useRouter()
-  return <div>      User {router.query.slug}</div>
-}
-
-export default User
--- src/app/users/layout.tsx
+++
@@ -1,5 +0,0 @@
-const LayoutUser = ({ children }: any) => {
-  return <div style={{ height: 100, width: 300, background: 'gold' }}>{children}</div>
-}
-
-export default LayoutUser
