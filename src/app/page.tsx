fbfc387433353fcbda472c97c98ee2ab719f193a
Nurzhan Zhakia <220760@astanait.edu.kz>
2025-01-30 17:18:13 +0500

    remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Header from '@/components/Header'
-import Intro from '@/components/Intro'
 import Section1 from '@/components/Section1'
 import Section2 from '@/components/Section2'
 import Section3 from '@/components/Section3'
